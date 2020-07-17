c1202c1f07002069cba42266fac2538a890ee199
daisuke sasaki <gerund25495641@gmail.com>
2020-07-17 23:16:42 +0900

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,15 @@
 class ArticlesController < ApplicationController
   def index
+<<<<<<< HEAD
+    @article = Article.all
+  end
+
+=======
     @articles = Article.all
   end
 
 
+>>>>>>> show-articles
   def show
     @article = Article.find(params[:id])
   end
